c64fc67b544042dbdc1b315ccc5f493a9a4c880b
Don P <donp@donp.org>
2020-11-18 16:32:35 -0800

    use ERC20 transferFrom to move coins into customer account

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.4 <0.8.0;
 
+import "./erc20/ERC20.sol";
+
 contract TokenBank {
 
   mapping (address => mapping (address => uint)) public balances;
 
   function addCoin(address erc20, uint256 amount) public {
+    ERC20 token = ERC20(erc20);
+    token.transferFrom(msg.sender, address(this), amount);
     balances[msg.sender][erc20] += amount;
+  }
+
+  function totalBalance(address erc20) public view returns (uint256) {
+    ERC20 token = ERC20(erc20);
+    uint256 balance = token.balanceOf(address(this));
+    return balance;
   }
 
   function takeCoin(address erc20, uint256 amount) public {
